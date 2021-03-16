7bdfa7984892a033038126dbb9ea2f26222001b4
alexisvedrenne482@gmail.com <alexisvedrenne482@gmail.com>
2021-03-16 17:35:22 +0100

    création du formulaire d'inscription début

--- src/Controller/InscriptionController.php
+++ src/Controller/InscriptionController.php
@@ -2,10 +2,18 @@
 
 namespace App\Controller;
 
+use App\Entity\Inscription;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Repository\InscriptionRepository;
+use App\Form\InscriptionType;
 
+/**
+ * @Route("/inscription",name="inscription_")
+ */
 class InscriptionController extends AbstractController
 {
     /**
@@ -17,4 +25,17 @@ class InscriptionController extends AbstractController
             'controller_name' => 'InscriptionController',
         ]);
     }
+
+    public function inscription(Request $request,EntityManagerInterface $manager,InscriptionRepository $repo)
+    {
+        $inscription=new Inscription();
+        $form=$this->createForm(InscriptionType::class,$inscription);
+        $form->handleRequest($request);
+        if($form->isSubmitted()&&$form->isValid()){
+            $manager->persist($inscription);
+            $manager->flush();
+        }
+        return $this->render('inscription/inscription.html.twig',['form'=>$form->createView(),]);
+    }
+
 }

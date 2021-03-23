7bd0efa68a5f16b656253b140069a596cdf2cd25
DUSSART Luke <lukedussart@hotmail.fr>
2021-03-23 14:44:20 +0100

    creation atelier form

--- src/Form/AtelierType.php
+++ src/Form/AtelierType.php
@@ -3,21 +3,34 @@
 namespace App\Form;
 
 use App\Entity\Atelier;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use App\Entity\Theme;
+use App\Entity\Vacation;
 
 class AtelierType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('libelle')
-            ->add('nbPlaceMaxi')
-            ->add('inscriptions')
-            ->add('themes')
-            ->add('vacations')
+            ->add('libelle', TextType::class)
+            ->add('nbPlaceMaxi', NumberType::class)
+            ->add('themes', EntityType::class,[
+                  'class' => Theme::class,
+                  'choice_label' => 'libelle',
+                  'expanded' => true,
+                  'multiple' => true,
+            ])
+            ->add('vacations', EntityType::class,[
+                'class' => Vacation::class,
+                'choice_label' => 'dateHeureDebut',
+                'expanded' => true,
+                'multiple' => true,
+            ])
         ;
     }
 

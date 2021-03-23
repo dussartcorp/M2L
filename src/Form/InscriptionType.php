a64858060968b2cda8d9da0acf8550d5f25650e8
alexisvedrenne482@gmail.com <alexisvedrenne482@gmail.com>
2021-03-23 17:04:01 +0100

    Ajout des restaurations dans l'inscription

--- src/Form/InscriptionType.php
+++ src/Form/InscriptionType.php
@@ -2,13 +2,14 @@
 
 namespace App\Form;
 
+use App\Entity\Restauration;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use App\Form\AteliersType;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use App\Form\NuiteType;
-
+use App\Form\RestaurationType;
 class InscriptionType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
@@ -17,6 +18,8 @@ class InscriptionType extends AbstractType
             ->add('ateliers',AteliersType::class)
             ->add('hotel1',NuiteType::class)
             ->add('hotel2',NuiteType::class)
+            ->add('restaurations1',RestaurationType::class)
+            ->add('restaurations2',RestaurationType::class)
         ;
     }
 
--- src/Form/RestaurationType.php
+++ src/Form/RestaurationType.php
@@ -6,14 +6,23 @@ use App\Entity\Restauration;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 
 class RestaurationType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('dateRestauration')
-            ->add('typesRepas')
+            ->add('dateRestauration',EntityType::class,[
+                'class'=>Restauration::class,
+                'choice_label'=>'dateRestauration',
+                'expanded'=>false,
+                'multiple'=>false,])
+            ->add('typesRepas',EntityType::class,[
+                'class'=>Restauration::class,
+                'choice_label'=>'typesRepas',
+                'expanded'=>true,
+                'multiple'=>true,])
         ;
     }
 

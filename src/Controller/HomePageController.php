4fbb8a9215c1c63b50692b7a6b008931f4b6f224
DUSSARTLuke <lukedussart@hotmail.fr>
2021-04-07 21:16:48 +0200

    code pour dateResto

--- src/Controller/HomePageController.php
+++ src/Controller/HomePageController.php
@@ -5,16 +5,19 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\RestaurationRepository;
 
 class HomePageController extends AbstractController
 {
     /**
      * @Route("/home", name="home")
      */
-    public function index(): Response
+    public function index(RestaurationRepository $repo): Response
     {
+        $date = $repo->getRestauration();
+        var_dump($date);
         return $this->render('base.html.twig', [
-            
+            'date' => $date
         ]);
     }
 }

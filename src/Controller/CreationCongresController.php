dcc1310ec3afb5b78ba441b6b7477ba8d05dfc65
DUSSARTLuke <lukedussart@hotmail.fr>
2021-04-21 21:42:34 +0200

    creation theme, atelier, vacation terminée

--- src/Controller/CreationCongresController.php
+++ src/Controller/CreationCongresController.php
@@ -10,11 +10,14 @@ use App\Repository\AtelierRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Doctrine\ORM\EntityManagerInterface;
 use App\Form\AtelierType;
+use App\Form\ThemeType;
+use App\Form\VacationType;
 use App\Entity\Theme;
+use App\Entity\Vacation;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
 /**
- * @Route("/congres/", name="congre_")
+ * @Route("/congres/", name="congres_")
  */
 class CreationCongresController extends AbstractController
 {
@@ -23,16 +26,110 @@ class CreationCongresController extends AbstractController
      */
     public function creationAtelier(Request $request, EntityManagerInterface $manager, AtelierRepository $repo){
         $atelier = new Atelier();
+        $theme = new Theme();
+        $vacation = new Vacation();
+
         $form = $this->createForm(AtelierType::class, $atelier);
+        $formTheme = $this->createForm(ThemeType::class, $theme);
+        $formVacation = $this->createForm(VacationType::class, $vacation);
+
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
         
-        $manager->persist($atelier);
-        $manager->flush();
-        return $this->redirectToRoute('home');
+            $manager->persist($atelier);
+            $manager->flush();
+            $this->addFlash('success', ' L\'atelier a bien été enregistré');
+            return $this->redirectToRoute('home');
         }
         return $this->render('creation_congres/creerAtelier.html.twig',
-            ['form' => $form->createView()]);
+            [
+                'form' => $form->createView(),
+                'formTheme' => $formTheme->createView(),
+                'formVacation' => $formVacation->createView()
+            ]
+        );
+    }
+
+    /**
+     * @Route("theme", name="theme")
+     */
+    public function creationTheme(Request $request, EntityManagerInterface $manager)
+    {
+        $atelier = new Atelier();
+        $theme = new Theme();
+        $vacation = new Vacation();
+
+        $form = $this->createForm(AtelierType::class, $atelier);
+        $formTheme = $this->createForm(ThemeType::class, $theme);
+        $formVacation = $this->createForm(VacationType::class, $vacation);
+
+        $formTheme->handleRequest($request);
+        if ($formTheme->isSubmitted() && $formTheme->isValid()) {
+
+            $manager->persist($theme);
+            $manager->flush();
+            $this->addFlash('success', ' Le thème a bien été enregistré');
+            return $this->redirectToRoute('congres_atelier');
+        }
+        return $this->render(
+            'creation_congres/creerAtelier.html.twig',
+            [
+                'form' => $form->createView(),
+                'formTheme' => $formTheme->createView(),
+                'formVacation' => $formVacation->createView()
+            ]
+        );
+    }
+
+    /**
+     * @Route("vacation", name="vacation")
+     */
+    public function creationVacation(Request $request, EntityManagerInterface $manager)
+    {
+        $atelier = new Atelier();
+        $theme = new Theme();
+        $vacation = new Vacation();
+
+        $form = $this->createForm(AtelierType::class, $atelier);
+        $formTheme = $this->createForm(ThemeType::class, $theme);
+        $formVacation = $this->createForm(VacationType::class, $vacation);
+
+        $formVacation->handleRequest($request);
+        if ($formVacation->isSubmitted() && $formVacation->isValid()) {
+
+            $dateDeb = $formVacation->get('dateHeureDebut')->getData();
+            $Deb = $dateDeb->format('d/n/Y');
+            $dateFin = $formVacation->get('dateHeureFin')->getData();
+            $fin = $dateFin->format('d/n/Y');
+            if ($dateDeb < $dateFin && $Deb === $fin) {
+
+                $mois_fr = array(
+                    "", "janvier", "février", "mars", "avril", "mai", "juin", "juillet", "août",
+                    "septembre", "octobre", "novembre", "décembre"
+                );
+
+                $hDeb = $dateDeb->format('H:i');
+                $hFin = $dateFin->format('H:i');
+                list($jour, $mois, $annee) = explode('/', $Deb);
+                $vacation->setLibelle("Le " . $jour . " " . $mois_fr[$mois] . " de " . $hDeb . " à " . $hFin);
+
+
+                $manager->persist($vacation);
+                $manager->flush();
+                $this->addFlash('success', ' La vacation a bien été enregistrée');
+                return $this->redirectToRoute('congres_atelier');
+            } else {
+                $this->addFlash('warning', 'La date de fin de la vacation n\'est pas conforme à la date de début');
+            }
+        }
+        return $this->render(
+            'creation_congres/creerAtelier.html.twig',
+            [
+                'form' => $form->createView(),
+                'formTheme' => $formTheme->createView(),
+                'formVacation' => $formVacation->createView()
+            ]
+        );
     }
 
 

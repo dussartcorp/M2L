36819351e5851127e110b4582433eac071f7af04
DUSSARTLuke <lukedussart@hotmail.fr>
2021-04-26 21:27:10 +0200

    changement nuitee controller

--- src/Controller/InscriptionController.php
+++ src/Controller/InscriptionController.php
@@ -55,23 +55,23 @@ class InscriptionController extends AbstractController
             
         }
         $inscription= New Inscription();
-        $Nuiteee1 = new Nuitee();
-        $Nuiteee2 = new Nuitee();
+        $Nuitee1 = new Nuitee();
+        $Nuitee2 = new Nuitee();
         $uneRestauration1=New Restauration();
         $uneRestauration2=New Restauration();
         $uneRestauration3=New Restauration();
-        $formNuiteee1 = $this->createForm(NuiteeType::class, $Nuiteee1);
-        $formNuiteee2 = $this->createForm(NuiteeType::class, $Nuiteee2);
+        $formNuitee1 = $this->createForm(NuiteeType::class, $Nuitee1);
+        $formNuitee2 = $this->createForm(NuiteeType::class, $Nuitee2);
         $form=$this->createForm(InscriptionType::class,$inscription);
         $form->handleRequest($request);
-        $formNuiteee1->handleRequest($request);
-        $formNuiteee2->handleRequest($request);
-        if (($form->isSubmitted() && $form->isValid()) && ($formNuiteee1->isSubmitted() && $formNuiteee1->isValid()) && ($formNuiteee2->isSubmitted() && $formNuiteee2->isValid())) {
-            if ($Nuiteee2->getHotel() == null && $Nuiteee1->getHotel() != null) {
-                $inscription->addNuiteee($Nuiteee1);
-            } else if ($Nuiteee2->getHotel() != null && $Nuiteee1->getHotel() != null) {
-                $inscription->addNuiteee($Nuiteee1);
-                $inscription->addNuiteee($Nuiteee2);
+        $formNuitee1->handleRequest($request);
+        $formNuitee2->handleRequest($request);
+        if (($form->isSubmitted() && $form->isValid()) && ($formNuitee1->isSubmitted() && $formNuitee1->isValid()) && ($formNuitee2->isSubmitted() && $formNuitee2->isValid())) {
+            if ($Nuitee2->getHotel() == null && $Nuitee1->getHotel() != null) {
+                $inscription->addNuitee($Nuitee1);
+            } else if ($Nuitee2->getHotel() != null && $Nuitee1->getHotel() != null) {
+                $inscription->addNuitee($Nuitee1);
+                $inscription->addNuitee($Nuitee2);
             }
             $inscription->setDateInscription(new DateTime('NOW'));
             if(isset($_POST['ckcSamMidi'])){
@@ -97,7 +97,7 @@ class InscriptionController extends AbstractController
             $manager->flush();
             return $this->redirectToRoute('home');
         }
-        return $this->render('inscription/inscriptionSejour.html.twig', ['form' => $form->createView(), 'Nuiteee1' => $formNuiteee1->createView(), 'Nuiteee2' => $formNuiteee2->createView(), 'resto1' => $resto1, 'resto2' => $resto2]);
+        return $this->render('inscription/inscriptionSejour.html.twig', ['form' => $form->createView(), 'nuitee1' => $formNuitee1->createView(), 'nuitee2' => $formNuitee2->createView(), 'resto1' => $resto1, 'resto2' => $resto2]);
     }
 
 

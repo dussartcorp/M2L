4706f0fa1ab9155b33bec1e4cf4c07be73b89fa0
DUSSARTLuke <lukedussart@hotmail.fr>
2021-05-03 21:04:15 +0200

    user fait, manque vacation

--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -4,11 +4,13 @@ namespace App\Controller\Admin;
 
 use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
-use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ArrayField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 
 class UserCrudController extends AbstractCrudController
 {
@@ -17,12 +19,22 @@ class UserCrudController extends AbstractCrudController
         return User::class;
     }
 
+    public function configureActions(Actions $actions): Actions
+    {
+        $details = Action::new('detailUser', 'Voir détails', 'fas fa-eye')
+        ->linkToCrudAction(Crud::PAGE_DETAIL)
+            ->addCssClass('btn btn-info');
+
+
+        return $actions
+            ->remove(Crud::PAGE_INDEX, Action::NEW)
+            ->add(Crud::PAGE_INDEX, $details);
+    }
 
 
 
     public function configureFields(string $pageName): iterable
     {
-        $mdp = TextField::new('password', 'Mot de passe');
 
         $fields = [
             IntegerField::new('numLicence', 'N° Licence'),
@@ -30,10 +42,12 @@ class UserCrudController extends AbstractCrudController
             ArrayField::new('roles')
         ];
 
-        if ($pageName === Crud::PAGE_EDIT || $pageName === Crud::PAGE_NEW) {
-            $fields[] = $mdp;
+        if ($pageName === Crud::PAGE_EDIT) {
+            return [
+                ArrayField::new('roles')
+            ];
+        } else {    
+            return $fields;
         }
-
-        return $fields;
     }
 }
--- src/Controller/Admin/VacationCrudController.php
+++ src/Controller/Admin/VacationCrudController.php
@@ -3,6 +3,10 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Vacation;
+use App\Repository\VacationRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
@@ -17,15 +21,62 @@ class VacationCrudController extends AbstractCrudController
         return Vacation::class;
     }
 
+
+    // /**
+    //  * @Route('/admin/saveVacation', name="createVacation")
+    //  */
+    // public function createVacation(Request $request, VacationRepository $vrepo, EntityManagerInterface $manager)
+    // {
+    //     $id = $request->query->get('id');
+    //     $dateHeureDeb = $request->query->get('dateHeureDebut');
+    //     $dateHeureFin = $request->query->get('dateHeureFin');
+
+
+    //     $vacation = $vrepo->find($id);
+    //     $Deb = $dateHeureDeb->format('d/n/Y');
+    //     $fin = $dateHeureFin->format('d/n/Y');
+    //     if ($dateHeureDeb < $dateHeureFin && $Deb === $fin) {
+
+    //         $mois_fr = array(
+    //             "", "janvier", "février", "mars", "avril", "mai", "juin", "juillet", "août",
+    //             "septembre", "octobre", "novembre", "décembre"
+    //         );
+
+    //         $hDeb = $dateHeureDeb->format('H:i');
+    //         $hFin = $dateHeureFin->format('H:i');
+    //         list($jour, $mois, $annee) = explode('/', $Deb);
+    //         $vacation->setLibelle("Le " . $jour . " " . $mois_fr[$mois] . " de " . $hDeb . " à " . $hFin);
+    //         $vacation->setDateHeureDebut($dateHeureDeb);
+    //         $vacation->setDateHeureFin($dateHeureFin);
+
+
+    //         $manager->persist($vacation);
+    //         $manager->flush();
+    //         $this->addFlash('success', ' La vacation a bien été enregistrée');
+    //     } else {
+    //         $this->addFlash('warning', 'La date de fin de la vacation n\'est pas conforme à la date de début');
+    //     }
+    // }
+
     public function configureActions(Actions $actions): Actions
     {
         // $details = Action::new('detailLicencie', 'Voir détails', 'fas fa-eye')
         // ->linkToCrudAction(Crud::PAGE_DETAIL)
         //     ->addCssClass('btn btn-info');
 
+        // $create = Action::new('create', 'Sauvegarder', 'fas fa_check')
+        // ->addCssClass('btn btn-info')
+        // ->linktoRoute("createVacation", function (Vacation $vacation) {
+        //     return [
+        //         'id' => $vacation->getId(),
+        //         'dateHeureDebut' => $vacation->getDateHeureDebut(),
+        //         'dateHeureFin' => $vacation->getDateHeureFin()
+        //     ];
+        // });
 
         return $actions
-            ->remove(Crud::PAGE_EDIT, Action::SAVE_AND_RETURN);
+        ->remove(Crud::PAGE_EDIT, Action::SAVE_AND_RETURN);
+        // ->add(Crud::PAGE_EDIT, $create);
         // ->remove(Crud::PAGE_INDEX, Action::NEW)
         // ->remove(Crud::PAGE_INDEX, Action::EDIT)
         // ->remove(Crud::PAGE_DETAIL, Action::EDIT)
